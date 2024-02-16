08a43978b650538a1f9631b63c8204318822f486
HaroRyu <89940456+HaroRyu@users.noreply.github.com>
2024-02-16 10:42:59 +0300

    [Offer] delete update

--- routes/offer.js
+++ routes/offer.js
@@ -30,6 +30,55 @@ router.post('/new', async (req, res) => {
     }
 });
 
+router.put('/update/:offerId', async (req, res) => {
+    try {
+        const { services, dateDebut, dateFin, description, reduction } = req.body;
+        const  offerId  = req.params.offerId;
+        console.log(offerId);
+        const dateD = new Date(dateDebut).toISOString();
+        const dateF = new Date(dateFin).toISOString();
+
+        const updatedOffer = await Offer.findByIdAndUpdate(
+            offerId,
+            {
+                services,
+                dateDebut: dateD,
+                dateFin: dateF,
+                description,
+                reduction
+            },
+            { new: true }
+        );
+
+        if (!updatedOffer) {
+            return res.status(404).json({ error: 'Offer not found' });
+        }
+
+        res.status(200).json({ message: 'Offer updated successfully', updatedOffer });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Update failed' });
+    }
+});
+
+router.delete('/delete/:offerId', async (req, res) => {
+    try {
+        const offerId = req.params.offerId;
+
+        const deletedOffer = await Offer.findByIdAndDelete(offerId);
+
+        if (!deletedOffer) {
+            return res.status(404).json({ error: 'Offer not found' });
+        }
+
+        res.status(200).json({ message: 'Offer deleted successfully', deletedOffer });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Deletion failed' });
+    }
+});
+
+
 router.get('/offers', async (req, res, next) => {
     var offers = await Offer.find();
     return res.status(200).json({ offers });
