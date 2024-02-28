306ba29fb0ceff7537d4ef53a4cec0f8458618e4
kenishiky <89154259+Radoharivola@users.noreply.github.com>
2024-02-28 12:09:13 +0300

    dbupdate

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb://0.0.0.0:27017/M1P11MEAN-ARO-KENNY';
+const MONGO_URI = 'mongodb+srv://admin:aZ12345678@m1p11mean-aro-kenny.zfdt81p.mongodb.net/?retryWrites=true&w=majority&appName=m1p11mean-Aro-Kenny';
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
