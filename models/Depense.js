1a99bdfa4bb562f28526f6c42a365a4be367ece2
kenishiky <89154259+Radoharivola@users.noreply.github.com>
2024-02-28 14:08:04 +0300

    jkop

--- models/Depense.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const depenseSchema = new mongoose.Schema({
-    date: { type: String, required: true },
-    motif: { type: String, required: true },
-    price: { type: Number, required: true },
-});
-module.exports = mongoose.model('Depense', depenseSchema);
--- models/Service.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const serviceSchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true },
-    price: { type: Number, required: true },
-    duration: { type: Number, required: true },
-    commission: { type: Number, required: true },
-    description: { type: String, required: true },
-});
-module.exports = mongoose.model('Service', serviceSchema);
